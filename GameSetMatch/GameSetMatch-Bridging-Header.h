efe455f0ef7262a961040d14da018c0d24de80ea
rictyran <rictyran@Richards-MacBook-Air.local>
2015-03-24 14:08:50 -0400

    Fixed name change debacle
    jo fixed it

--- GameSetMatch/GameSetMatch-Bridging-Header.h
+++ GameSetMatch/GameSetMatch-Bridging-Header.h
@@ -1,5 +1,7 @@
+#import <Parse/Parse.h>
+#import <Bolts/Bolts.h>
+
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import<Parse/Parse.h>
